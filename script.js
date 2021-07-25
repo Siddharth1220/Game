80f227f0f739523373e685bc7ca8f1d2e25748f8
Siddharth Vaniya <Tonytech1220@gmail.com>
2021-07-25 11:57:41 +0530

    game using htm,css and javascript

---
+++ script.js
@@ -0,0 +1,27 @@
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+var counter=0;
+function jump(){
+    if(character.classList == "animate"){return}
+    character.classList.add("animate");
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },300);
+}
+var checkDead = setInterval(function() 
+{
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft<5 && blockLeft>-20 && characterTop>=150)
+    {
+        block.style.animation = "none";
+        alert(" Game Over.\nSiddharth Vaniya\nscore: "+Math.floor(counter/200));
+        counter=0;
+        block.style.animation = "block 2s infinite linear";
+    }
+    else
+    {
+        counter++;
+        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/200);
+    }
+}, 1);
